de7896f39b37e61794934256a90a5b25b72b8524
aniketverma11 <aniketverma1103@gmail.com>
2025-10-15 06:12:59 +0000

    changes

--- task_management/settings.py
+++ task_management/settings.py
@@ -40,6 +40,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "http://135.235.193.77:8000"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
